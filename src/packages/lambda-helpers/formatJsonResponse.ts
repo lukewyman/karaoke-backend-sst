1b4432ced17fc102ad615d113c20b3a0aadfda41
Luke Wyman <lukewyman@gmail.com>
2021-08-30 17:14:49 -0600

    Cleaned up extraneous typescript/lerna stuff after python refactor

--- src/packages/lambda-helpers/formatJsonResponse.ts
+++
@@ -1,8 +0,0 @@
-const formatJSONResponse = (statusCode: number, response: any): any => {
-  return {
-    statusCode,
-    body: JSON.stringify(response),
-  };
-};
-
-export default formatJSONResponse;
--- src/packages/lambda-helpers/index.ts
+++
@@ -1,4 +0,0 @@
-import middify from './middify';
-import formatJSONResponse from './formatJsonResponse';
-
-export { middify, formatJSONResponse };
