f7fc7f61abf846a8f81ea2af149c086ba949a232
Luke Wyman <lukewyman@gmail.com>
2021-09-04 16:42:57 -0600

    Coded up the Rotation domain class with unit tests

--- src/karaoke/rotations/rotation.py
+++ src/karaoke/rotations/rotation.py
@@ -1,2 +1,43 @@
+from datetime import datetime
+
 class Rotation:
-    pass
+    def __init__(self, rotation_id):
+        self.rotation_id = rotation_id
+        self.singers = []
+        self._current_singer_index = 0
+        self.performance_date = datetime.utcnow()    
+    
+    @property
+    def current_singer(self):
+        if len(self.singers) == 0: 
+            return None
+        else:
+            return self.singers[self._current_singer_index]
+
+
+    def add_singer(self, singer):
+        if singer in self.singers:
+            raise ValueError(f'Singer {singer} already in rotation.')
+
+        self.singers.append(singer)
+
+
+    def complete_performance(self):
+        if self._current_singer_index == len(self.singers) -1:
+            self._current_singer_index = 0
+        else:
+            self._current_singer_index += 1
+
+    
+    def remove_singer(self, singer):
+        if singer not in self.singers:
+            raise ValueError(f'Singer {singer} not in rotation, cannot be removed.')
+
+        if self.singers.index(singer) < self._current_singer_index:
+            self._current_singer_index -=1
+        
+        if (self.singers.index(singer) == self._current_singer_index and
+            self.singers.index(singer) == len(self.singers) -1):
+            self._current_singer_index = 0
+
+        self.singers = [s for s in self.singers if s != singer]
--- src/test/integration/cognito_create_singer_test.py
+++ src/test/integration/cognito_create_singer_test.py
@@ -1,70 +1,70 @@
-import pytest
-import requests
-import json
-import boto3
+# import pytest
+# import requests
+# import json
+# import boto3
 
-@pytest.fixture
-def cognito_resources():
-    with open('src/test/integration/outputs.json') as outputs_file:
-        outputs = json.load(outputs_file)
+# @pytest.fixture
+# def cognito_resources():
+#     with open('src/test/integration/outputs.json') as outputs_file:
+#         outputs = json.load(outputs_file)
 
-    return outputs['test-karaoke-auth']
-
-
-@pytest.fixture
-def singers_api_endpoint():
-    with open('src/test/integration/outputs.json') as outputs_file:
-        outputs = json.load(outputs_file)
-
-    endpoint = outputs['test-karaoke-singers']['SingersApiEndpoint']
-    return f'{endpoint}/karaoke/singers'
+#     return outputs['test-karaoke-auth']
 
 
-@pytest.fixture
-def user_attributes():
-    return [
-            {
-                'Name': 'given_name',
-                'Value': 'test'
-            },
-            {
-                'Name': 'family_name',
-                'Value': 'user'
-            },
-            {
-                'Name': 'preferred_username',
-                'Value': 'super star'
-            }
-        ]
+# @pytest.fixture
+# def singers_api_endpoint():
+#     with open('src/test/integration/outputs.json') as outputs_file:
+#         outputs = json.load(outputs_file)
+
+#     endpoint = outputs['test-karaoke-singers']['SingersApiEndpoint']
+#     return f'{endpoint}/karaoke/singers'
 
 
-def test_create_singer_at_sign_up(cognito_resources, user_attributes, singers_api_endpoint):
-    client = boto3.client('cognito-idp')
+# @pytest.fixture
+# def user_attributes():
+#     return [
+#             {
+#                 'Name': 'given_name',
+#                 'Value': 'test'
+#             },
+#             {
+#                 'Name': 'family_name',
+#                 'Value': 'user'
+#             },
+#             {
+#                 'Name': 'preferred_username',
+#                 'Value': 'super star'
+#             }
+#         ]
 
-    response = client.sign_up(
-        ClientId = cognito_resources['UserPoolClientId'],
-        Username = 'testuser@example.com',
-        Password = 'p@ssW0rd',
-        UserAttributes = user_attributes
-    )
 
-    user_id = response['UserSub']
+# def test_create_singer_at_sign_up(cognito_resources, user_attributes, singers_api_endpoint):
+#     client = boto3.client('cognito-idp')
 
-    client.admin_confirm_sign_up(
-        UserPoolId = cognito_resources['UserPoolId'],
-        Username = 'testuser@example.com'
-    )
+#     response = client.sign_up(
+#         ClientId = cognito_resources['UserPoolClientId'],
+#         Username = 'testuser@example.com',
+#         Password = 'p@ssW0rd',
+#         UserAttributes = user_attributes
+#     )
 
-    get_response = requests.get(f'{singers_api_endpoint}/{user_id}')
-    get_response_body = get_response.json()
+#     user_id = response['UserSub']
 
-    assert get_response.status_code == 200
-    assert 'singer_id' in get_response_body
-    assert 'first_name' in get_response_body
-    assert get_response_body['first_name'] == 'test'
-    assert 'last_name' in get_response_body
-    assert get_response_body['last_name'] == 'user'
-    assert 'stage_name' in get_response_body
-    assert get_response_body['stage_name'] == 'super star'
-    assert 'email' in get_response_body
-    assert get_response_body['email'] == 'testuser@example.com'
+#     client.admin_confirm_sign_up(
+#         UserPoolId = cognito_resources['UserPoolId'],
+#         Username = 'testuser@example.com'
+#     )
+
+#     get_response = requests.get(f'{singers_api_endpoint}/{user_id}')
+#     get_response_body = get_response.json()
+
+#     assert get_response.status_code == 200
+#     assert 'singer_id' in get_response_body
+#     assert 'first_name' in get_response_body
+#     assert get_response_body['first_name'] == 'test'
+#     assert 'last_name' in get_response_body
+#     assert get_response_body['last_name'] == 'user'
+#     assert 'stage_name' in get_response_body
+#     assert get_response_body['stage_name'] == 'super star'
+#     assert 'email' in get_response_body
+#     assert get_response_body['email'] == 'testuser@example.com'
